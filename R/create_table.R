6fe3b2328c09242b49c76490a7d1c10b3bb13863
Bradley Buchsbaum <bbuchsbaum@Donacobius-151.local>
2018-12-12 12:25:24 -0500

    factor out atlas method

--- R/create_table.R
+++ R/create_table.R
@@ -1,14 +1,10 @@
 
-#' @export
-create_table <- function(im, threshold=0, local_maxima=TRUE, local_maxima_dist=10, statname="z-stat", ...) {
-  ccomp <- conn_comp(im, local_maxima_dist=local_maxima_dist)
-  
-  local_maxima <- as.data.frame(ccomp$local_maxima)
-  vox <- ccomp$local_maxima[,2:4]
-  values <- ccomp$local_maxima[,5]
-  area <- ccomp$cluster_table$Area[local_maxima$index]
-  
-  cds <- grid_to_coord(im, vox)
+
+harvard_oxford <- function(cds) {
+  cort_labels <- read.table(system.file("extdata", "cortical_table.txt", 
+                                        package = "mnitables"), header=TRUE)
+  subcort_labels <- read.table(system.file("extdata", "subcortical_table.txt", 
+                                           package = "mnitables"), header=TRUE)
   
   cort_atlas <- 
     neuroim2::read_vol(system.file("extdata", "HarvardOxford-cort-maxprob-thr0-2mm.nii.gz", 
@@ -18,12 +14,7 @@ create_table <- function(im, threshold=0, local_maxima=TRUE, local_maxima_dist=1
     neuroim2::read_vol(system.file("extdata", "HarvardOxford-sub-maxprob-thr0-2mm.nii.gz", 
                                    package = "mnitables"))
   
-  cort_labels <- read.table(system.file("extdata", "cortical_table.txt", 
-                                        package = "mnitables"), header=TRUE)
-  subcort_labels <- read.table(system.file("extdata", "subcortical_table.txt", 
-                                        package = "mnitables"), header=TRUE)
-  
-  vox2 <- coord_to_grid(atlas, cds)
+  vox2 <- coord_to_grid(cort_atlas, cds)
   
   ltab <- do.call(rbind, lapply(1:nrow(vox2), function(i) {
     v <- vox2[i,]
@@ -50,6 +41,22 @@ create_table <- function(im, threshold=0, local_maxima=TRUE, local_maxima_dist=1
     data.frame(x=cds[i,1], y=cds[i,2], z=cds[i,3], Hemi=hemi, Label=label)
   }))
   
+  
+}
+
+
+#' @export
+create_table <- function(im, threshold=0, local_maxima=TRUE, local_maxima_dist=10, statname="z-stat", ...) {
+  ccomp <- conn_comp(im, local_maxima_dist=local_maxima_dist)
+  
+  local_maxima <- as.data.frame(ccomp$local_maxima)
+  vox <- ccomp$local_maxima[,2:4]
+  values <- ccomp$local_maxima[,5]
+  area <- ccomp$cluster_table$Area[local_maxima$index]
+  
+  cds <- grid_to_coord(im, vox)
+  
+  ltab <- hardvard_oxford(cds)
   ltab[[statname]] <- values
   ltab$Area <- area
   ltab
